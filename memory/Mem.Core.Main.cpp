6c84b0d8d3e02d05b8e0db3bb41cad9711266386
Theo <theowimber@gmail.com>
2025-09-27 22:13:28 +0200

    initial project structure

---
+++ memory/Mem.Core.Main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by theow on 27.09.2025.
+//
+
+#include "Mem.Core.Main.h"
---
+++ memory/Mem.Core.Main.h
@@ -0,0 +1,8 @@
+//
+// Created by theow on 27.09.2025.
+//
+
+#ifndef MEM_CORE_MAIN_H
+#define MEM_CORE_MAIN_H
+
+#endif //MEM_CORE_MAIN_H
